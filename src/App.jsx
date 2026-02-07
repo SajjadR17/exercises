495fc3a4061c09464c39a23d36dd1f44f08e8de2
Sajjad Roohandeh <Roohandehsredi6@gmail.com>
2026-02-07 20:16:10 +0330

    Render cards with props in React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./Exercise";
+
+function App() {
+  return (
+    <>
+      <div className="info">
+        <h2>Sajjad Roohandeh</h2>
+        <span>React Exercises</span>
+      </div>
+      <Card title="React" desc="JS Library"/>
+      <Card title="Html" desc="Hyper Text Markup Language" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Exercise.jsx
@@ -0,0 +1,12 @@
+function Card({ title, desc }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{desc}</p>
+    </div>
+  );
+}
+
+export default Card;
+
+
